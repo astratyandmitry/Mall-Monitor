2c9f49d908ad58425cdc7251cfdeaa9f624a5107
astratyandmitry <astratyandmitry@gmail.com>
2018-12-25 11:59:03 +0600

    Add Kernel task.
    Took 1 minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\Import\IntegrateProsystemsMultiCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        'mallmonitor:integrate-prosystems-multi' => IntegrateProsystemsMultiCommand::class,
     ];
 
 
@@ -27,7 +28,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('inspire')->hourly();
+        $schedule->command('mallmonitor:integrate-prosystems-multi')->hourly();
     }
 
 
