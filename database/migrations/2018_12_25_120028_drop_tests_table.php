9956d4c059286bfd0d9793b6c1ed3825ed581263
astratyandmitry <astratyandmitry@gmail.com>
2018-12-25 12:02:09 +0600

    Drop tests table.
    Took 3 minutes

---
+++ database/migrations/2018_12_25_120028_drop_tests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropTestsTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('tests');
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('tests', function (Blueprint $table): void {
+            $table->increments('id');
+            $table->longText('body');
+            $table->enum('method', ['GET', 'POST']);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+}
