0d7f974d0039a118afda00abf8c84ffab7c14740
astratyandmitry <astratyandmitry@gmail.com>
2019-04-11 10:23:10 +0600

    Fixes.

--- resources/views/stores/index.blade.php
+++ resources/views/stores/index.blade.php
@@ -14,18 +14,19 @@
         </div>
     </div>
 
-    <div class="content">
-        <div class="container">
-            <div class="stores">
-                @foreach($stores as $store)
-                    @php $total = (isset($statistics[$store->id])) ? number_format(round($statistics[$store->id])) : 0 @endphp
+    @if (count($stores))
+        <div class="content">
+            <div class="container">
+                <div class="stores">
+                    @foreach($stores as $store)
+                        @php $total = (isset($statistics[$store->id])) ? number_format(round($statistics[$store->id])) : 0 @endphp
 
-                    <a href="{{ $store->link() }}" class="stores-item {{ $store->is_errors_yesterday ? 'is-danger' : '' }}">
-                        <div class="stores-item-name">
-                            <span class="stores-item-name-text">{{ $store->name }}</span>
-                        </div>
+                        <a href="{{ $store->link() }}" class="stores-item {{ $store->is_errors_yesterday ? 'is-danger' : '' }}">
+                            <div class="stores-item-name">
+                                <span class="stores-item-name-text">{{ $store->name }}</span>
+                            </div>
 
-                        <div class="stores-item-detail">
+                            <div class="stores-item-detail">
                             <span class="stores-item-detail-text">
                                 @if (!$store->is_errors_yesterday)
                                     Текущая выручка: {{ $total }} ₸
@@ -33,10 +34,21 @@
                                     Отсутствуют вчерашние транзакции
                                 @endif
                             </span>
-                        </div>
-                    </a>
-                @endforeach
+                            </div>
+                        </a>
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
+    @else
+        <div class="information">
+            <div class="container">
+                <div class="information-box is-lg">
+                    <div class="information-box-text">
+                        Информация по указанному запросу отсутствует
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 @endsection
