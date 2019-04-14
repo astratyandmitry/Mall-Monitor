42a310bbf7db58982d56500d50ecd4634befae85
astratyandmitry <astratyandmitry@gmail.com>
2019-04-14 20:13:14 +0600

    Fixes.

--- resources/views/layouts/includes/field/hidden.blade.php
+++ resources/views/layouts/includes/field/hidden.blade.php
@@ -1,4 +1,4 @@
 <input
         type="hidden" name="{{ $attribute }}" id="{{ $attribute }}"
-        value="{{ isset($_GET[$attribute]) ? $_GET[$attribute] : null }}"
+        value="{{ isset($_GET[$attribute]) ? $_GET[$attribute] : @$value }}"
 />
