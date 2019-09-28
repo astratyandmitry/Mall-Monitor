fe00d57025f1a4e45eaa6e40aa52eab4e4eff0e4
Astratyan Dmitry <astratyandmitry@gmail.com>
2019-09-28 22:14:25 +0600

    Fix routes.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
+Route::group(['namespace' => 'API'], function (): void {
+    Route::post('/visits/import/excel', 'VisitsImportExcelController');
+});
+
 Route::group(['middleware' => 'auth.basic:api,username', 'namespace' => 'API'], function (): void {
     Route::post('/cheques/import/xml', 'ChequesImportXMLController');
     Route::post('/cheques/import/excel', 'ChequesImportExcelController');
-    Route::post('/visits/import/excel', 'VisitsImportExcelController');
 });
