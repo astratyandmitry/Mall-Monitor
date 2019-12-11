e1302714089b3337e34c071b45948d619a4e9beb
Astratyan Dmitry <astratyandmitry@gmail.com>
2019-12-11 10:33:39 +0600

    Fix

--- resources/views/stores/index.blade.php
+++ resources/views/stores/index.blade.php
@@ -1,4 +1,4 @@
-@php /** @var array $statistics */ @endphp
+@php /** @var array $stats */ @endphp
 @php /** @var array $visits */ @endphp
 @php /** @var \App\Models\Store[] $stores */ @endphp
 
@@ -20,7 +20,7 @@
             <div class="container">
                 <div class="stores">
                     @foreach($stores as $store)
-                        @php $storeItem = new App\Classes\Design\StoreItem(@$statistics[$store->id], @$visits[$store->id]) @endphp
+                        @php $storeItem = new App\Classes\Design\StoreItem(@$stats[$store->id], @$visits[$store->id]) @endphp
 
                         <a href="{{ $store->link() }}" class="stores-item {{ $store->is_errors_yesterday ? 'is-danger' : '' }}">
                             @if ($store->rentable_area)
