f7fb592cec188ba02ca326d4c694eb488a2814e9
Astratyan Dmitry <astratyandmitry@gmail.com>
2019-12-11 17:19:06 +0600

    Fix

--- resources/views/placement/includes/placement-table-td.blade.php
+++ resources/views/placement/includes/placement-table-td.blade.php
@@ -12,7 +12,7 @@
 <td nowrap class="is-right {{ placement_background($_diff) }}">
     <span class="period">тек.:</span> {{ number_format($_current) }} {{ $_currency ? '₸' : '' }}<br/>
     <span class="period">пред.:</span> {{ number_format($_past) }} {{ $_currency ? '₸' : '' }}<br/>
-    @if ($_current != $_past)
+    @if ($_current != $_past && ! ($_current == 0 && $_past == 0))
         <strong class="{{ placement_color($_diff) }}">{{ $_diff }}% <i class="fa fa-arrow-{{ placement_arrow($_diff) }}"></i></strong>
     @endif
 </td>
